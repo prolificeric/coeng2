b1f791c551c4e26e52d1fd0a6a1ce3a0510fe84f
Eric Weber <eweb04@gmail.com>
2023-12-12 19:50:38 -0500

    Add AstNode methods

--- packages/core/lang/ast.ts
+++ packages/core/lang/ast.ts
@@ -5,38 +5,76 @@ export type AstNodeType =
   | 'BRANCH'
   | 'COMPOUND'
   | 'ATOM'
-  | 'PARENTHESIS';
+  | 'INLINE_BRANCHING'
+  | 'PARENTHETICAL';
 
 export type AstNodeInit = {
   type: AstNodeType;
   children?: AstNode[];
   parent?: AstNode;
+  prev?: AstNode;
+  next?: AstNode;
   token?: Token;
 };
+
+export type Predicate = string | ((node: AstNode) => boolean);
 
 export class AstNode {
   type: AstNodeType;
   children: AstNode[];
-  parent?: AstNode;
-  token?: Token;
+  parent: AstNode | null;
+  prev: AstNode | null;
+  next: AstNode | null;
+  token: Token | null;
 
   constructor(init: AstNodeInit) {
     this.type = init.type;
     this.children = init.children || [];
-    this.token = init.token;
-    this.parent = init.parent;
+    this.parent = init.parent || null;
+    this.prev = init.prev || null;
+    this.next = init.next || null;
+    this.token = init.token || null;
   }
 
-  requireParent() {
-    if (!this.parent) {
-      throw new Error('No parent');
+  static parse(tokens: Token[]): AstNode {
+    const root = new AstNode({ type: 'BRANCH_LIST' });
+    let cursor = root.createChild({ type: 'BRANCH' });
+
+    for (const token of tokens) {
+      const handler = astTokenHandlers[token.type];
+
+      if (!handler) {
+        throw new Error(`No handler for token type ${token.type}`);
+      }
+
+      cursor = handler(token, cursor);
     }
 
-    return this.parent;
+    return root;
   }
 
-  getLastChild() {
-    return this.children?.at(-1) || null;
+  get parentOrSelf(): AstNode {
+    return this.parent || this;
+  }
+
+  insertBefore(node: AstNode): AstNode {
+    node.insertAfter(this);
+    return this;
+  }
+
+  insertAfter(node: AstNode): AstNode {
+    if (node.prev) {
+      node.prev.next = node.next;
+    }
+
+    if (node.next) {
+      node.next.prev = node.prev;
+    }
+
+    this.next = node;
+    node.prev = this;
+
+    return this;
   }
 
   createChild(init: AstNodeInit): AstNode {
@@ -44,117 +82,137 @@ export class AstNode {
       ...init,
       parent: this,
     });
+
     this.children.push(child);
+
     return child;
   }
-}
 
-export type AstParseCursorInit = {
-  root: AstNode;
-  branch: AstNode;
-  prev?: AstNode;
-};
-
-export class AstParseCursor {
-  root: AstNode;
-  branch: AstNode;
-  prev: AstNode | null;
-
-  constructor(init: AstParseCursorInit) {
-    this.root = init.root;
-    this.branch = init.branch;
-    this.prev = init.prev || null;
-  }
-
-  createBranchChild(init: AstNodeInit) {
-    this.prev = this.branch.createChild(init);
-  }
-
-  nextBranch() {
-    this.branch = this.branch.requireParent().createChild({ type: 'BRANCH' });
-    this.prev = null;
-  }
-
-  closeBranchList() {}
-
-  getParentBranchList() {}
-
-  goUpUntil(test: (node: AstNode) => boolean) {}
-}
-
-export const parseAst = (tokens: Token[]): AstNode => {
-  const root = new AstNode({ type: 'BRANCH_LIST' });
-
-  const cursor = new AstParseCursor({
-    root,
-    prev: root,
-    branch: root.createChild({ type: 'BRANCH' }),
-  });
-
-  for (const token of tokens) {
-    const handler = astTokenHandlers[token.type];
-
-    if (!handler) {
-      throw new Error(`No handler for token type ${token.type}`);
+  requireParent(): AstNode {
+    if (this.parent) {
+      return this.parent;
     }
 
-    handler(token, cursor);
+    throw new Error('Node has no parent');
   }
 
-  return root;
-};
+  requireAncestor(predicate: Predicate): AstNode {
+    return this.traverseOrThrow(
+      node => node.parent,
+      predicate,
+      'Could not find ancestor',
+    );
+  }
+
+  requirePrev(predicate: Predicate): AstNode {
+    return this.traverseOrThrow(
+      node => node.prev,
+      predicate,
+      'Could not find next node',
+    );
+  }
+
+  requireNext(predicate: Predicate): AstNode {
+    return this.traverseOrThrow(
+      node => node.parent,
+      predicate,
+      'Could not find next node',
+    );
+  }
+
+  findAncestor(predicate: Predicate): AstNode | null {
+    return this.traverse(node => node.parent, predicate);
+  }
+
+  findPrev(predicate: Predicate) {
+    return this.traverse(node => node.prev, predicate);
+  }
+
+  findNext(predicate: Predicate) {
+    return this.traverse(node => node.next, predicate);
+  }
+
+  traverse(selector: (node: AstNode) => AstNode | null, predicate: Predicate) {
+    let node: AstNode | null = this;
+
+    if (typeof predicate === 'string') {
+      const type = predicate;
+      predicate = node => node.type === type;
+    }
+
+    while ((node = selector(node))) {
+      if (predicate(node)) {
+        return node;
+      }
+    }
+
+    return null;
+  }
+
+  traverseOrThrow(
+    selector: (node: AstNode) => AstNode | null,
+    predicate: Predicate,
+    errorMessage: string,
+  ) {
+    const node = this.traverse(selector, predicate);
+
+    if (!node) {
+      throw new Error(errorMessage);
+    }
+
+    return node;
+  }
+}
 
 export const astTokenHandlers: Record<
   TokenType,
-  (token: Token, cursor: AstParseCursor) => void
+  (token: Token, cursor: AstNode) => AstNode
 > = {
   ATOM(token, cursor) {
-    cursor.branch.createChild({ type: 'ATOM', token });
+    throw new Error('Function not implemented.');
   },
 
   BRANCH_SEPARATOR(token, cursor) {
-    cursor.branch = cursor.branch
-      .requireParent()
-      .createChild({ type: 'BRANCH' });
+    throw new Error('Function not implemented.');
   },
 
-  PART_SEPARATOR() {
-    // No-op
+  PART_SEPARATOR(token, cursor) {
+    throw new Error('Function not implemented.');
   },
 
   L_PAREN(token, cursor) {
-    // const
-  },
-
-  R_PAREN(token) {
     throw new Error('Function not implemented.');
   },
 
-  L_CURLY(token) {
+  R_PAREN(token, cursor) {
     throw new Error('Function not implemented.');
   },
 
-  R_CURLY(token) {
+  L_CURLY(token, cursor) {
     throw new Error('Function not implemented.');
   },
 
-  L_SQUARE(token) {
+  R_CURLY(token, cursor) {
     throw new Error('Function not implemented.');
   },
 
-  R_SQUARE(token) {
+  L_SQUARE(token, cursor) {
     throw new Error('Function not implemented.');
   },
 
-  HEAD_REF(token) {
+  R_SQUARE(token, cursor) {
     throw new Error('Function not implemented.');
   },
 
-  PREV_REF(token) {
+  HEAD_REF(token, cursor) {
     throw new Error('Function not implemented.');
   },
 
-  SORTED_SET_INIT(token) {
+  PREV_REF(token, cursor) {
+    throw new Error('Function not implemented.');
+  },
+
+  SORTED_SET_INIT(token, cursor) {
     throw new Error('Function not implemented.');
   },
 };
